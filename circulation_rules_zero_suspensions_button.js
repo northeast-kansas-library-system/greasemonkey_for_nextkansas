a3b4bc292d06b86c3e9b33f17ac733927d1b1d43
will1410 <gwilliams@nekls.org>
2021-10-29 08:45:33 -0500

    Fix flaws in upload file and add new version number

--- circulation_rules_zero_suspensions_button.js
+++ circulation_rules_zero_suspensions_button.js
@@ -2,13 +2,12 @@
 // @name           Koha circulation rules-zero suspensions
 // @description    Creates a button to set all suspension values to zero the fines and associated fields in a row being edited in the circulation rules matrix
 // @author         George H. Williams
-// @version        1
-// @updateURL      https://raw.githubusercontent.com/northeast-kansas-library-system/greasemonkey_for_nextkansas/main/circulation_rules_zero_suspensions_button.js
-// @downloadURL    https://raw.githubusercontent.com/northeast-kansas-library-system/greasemonkey_for_nextkansas/main/circulation_rules_zero_suspensions_button.js
+// @version        1.1
 // @grant          none
 // @match          https://staff.nextkansas.org/*
 // @match          http://staff-test.nexpresslibrary.org/*
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @downloadURL https://raw.githubusercontent.com/northeast-kansas-library-system/greasemonkey_for_nextkansas/main/circulation_rules_zero_suspensions_button.js
 // ==/UserScript==
 
 $(document).ready(function() {
