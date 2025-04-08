f7afd3ff057434b0ca049a3ee51850a936e3e80d
George Williams <will1410@users.noreply.github.com>
2025-04-08 14:22:30 -0500

    Add private_video script

---
+++ private_video.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name           Koha/Aspen private video
+// @description    Hide some fields during training video creation
+// @author         George H. Williams
+// @version        1.1
+// @grant          none
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @match          https://staff.nekls.bywatersolutions.com/*
+// @match          https://staff.nekls-test.bywatersolutions.com/*
+// @match          https://*.nextkansas.org/*
+// @match          https://nekls-digital.aspendiscovery.org/*
+// ==/UserScript==
+
+/* ========== Turn on this script when recording trainings that may contain confidential data ========== */
+
+$(document).ready(function () {
+
+//Aspen Discovery
+
+  //Hides nsc drop-down
+    $('#nsc_dropdown').hide();
+    
+  //shows #nsc_dropdown when header is hovered on
+    $('#header-wrapper').hover(function() { 
+      $('#nsc_dropdown').show();
+    }, function() { 
+      $('#nsc_dropdown').hide(); 
+    }); 
+
+  //Masks logged in user name on all pages
+    $('#account-menu-dropdown > span:nth-child(2)').text('YOUR USERNAME');
+    
+  //Hides confidential content in "Sign in" modal
+    $('#username').val('ENTER YOUR USERNAME HERE');
+    $('#password').removeAttr('type').val('ENTER YOUR PASSWORD HERE');
+
+  //Hides confidential content on  Home > Browse > Your Account > Contact Information (/MyAccount/ContactInformation)
+    $('#MyAccount-ContactInformation #main-content input').attr('readonly', 'true').val('CONFIDENTIAL - Not shown in training videos');
+
+  //Hides confidential content on Home > Browse > Login (/MyAccount/Home)
+    $('#account-menu-dropdown span').html('Your username').attr('style','padding-right: 10px'); 
+    $('#pin').removeAttr('type').val('YOUR PASSWORD');
+    $('#pin1').removeAttr('type').val('ENTER YOUR NEW PASSWORD');
+    $('#pin2').removeAttr('type').val('ENTER YOUR NEW PASSWORD AGAIN');
+    
+  //Hides confidential content on Home > Browse > Administration Home > System Administration > Administrators (/Admin/Administrators)
+    $('#adminTable td[aria-label$="name"], #adminTable td[aria-label~="displayName"], #adminTable td[aria-label~="ils_barcode"]').text('Confidential');
+
+});
